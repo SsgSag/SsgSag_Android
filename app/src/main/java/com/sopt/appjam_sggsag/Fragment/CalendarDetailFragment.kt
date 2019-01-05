1db8656d047a18cc53393c5ed7750e716f1526e8
jineeKim <jin03hee16@gmail.com>
2019-01-06 08:23:58 +0900

    고쳤당

--- app/src/main/java/com/sopt/appjam_sggsag/Fragment/CalendarDetailFragment.kt
+++ app/src/main/java/com/sopt/appjam_sggsag/Fragment/CalendarDetailFragment.kt
@@ -4,100 +4,62 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.sopt.appjam_sggsag.Adapter.CalendarRecyclerAdapter
+import com.sopt.appjam_sggsag.Data.CalendarDateData
+import com.sopt.appjam_sggsag.Data.EventList
+import com.sopt.appjam_sggsag.MyApplication
 
 import com.sopt.appjam_sggsag.R
+import kotlinx.android.synthetic.main.fragment_calendar_detail.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class CalendarDetailFragment : Fragment(){
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [CalendarDetailFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [CalendarDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
-class CalendarDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
+    lateinit var recyclerViewAdapter: CalendarRecyclerAdapter
+
+    //var dataList = (activity!!.application as MyApplication).dataList1
+    //(activity!!.application as MyApplication).
+    var dataList : ArrayList<CalendarDateData> = ArrayList()
+    var scheduleList : ArrayList<EventList> = ArrayList()
+    var month: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            month = it.getInt("diff")
+
         }
+
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar_detail, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: View = inflater.inflate(R.layout.fragment_calendar_detail, container, false)
+        //   toast(month.toString())
+        return view
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        dataList = (activity!!.application as MyApplication).dataList1
+        scheduleList = (activity!!.application as MyApplication).eventList1
+        setRecycleView()
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-        }
+    private fun setRecycleView() {
+        //임시데이터
+
+
+
+        //리사이클러뷰 어댑터를 만들어서 아래처럼 고대로 하면 돼! 그 리사이클러뷰 어댑터에서 ctx는 activity!! 이거 넘겨주면되고!
+        recyclerViewAdapter = CalendarRecyclerAdapter(activity!!, dataList,scheduleList, month)
+        frag_calendar_detail_recycle_view.adapter = recyclerViewAdapter
+        frag_calendar_detail_recycle_view.layoutManager = GridLayoutManager(getActivity(), 7)
+
+
     }
 
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CalendarDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CalendarDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
