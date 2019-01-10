6a3ef63690e2dd84b4f505a42a5102262cae6024
jineeKim <jin03hee16@gmail.com>
2019-01-10 20:18:41 +0900

    career를 끝내버리고 싶다

--- app/src/main/java/com/sopt/appjam_sggsag/Fragment/Career/Career1Fragment.kt
+++ app/src/main/java/com/sopt/appjam_sggsag/Fragment/Career/Career1Fragment.kt
@@ -3,62 +3,60 @@ package com.sopt.appjam_sggsag.Fragment.Career
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.ImageView
 import android.widget.RelativeLayout
-import android.widget.TextView
 import com.sopt.appjam_sggsag.Adapter.Career.CareerRecyclerViewAdapter
 import com.sopt.appjam_sggsag.Career.OutActivityDetail
 import com.sopt.appjam_sggsag.Data.Career.CareerListData
 import com.sopt.appjam_sggsag.R
-import kotlinx.android.synthetic.main.activity_out_detail.*
 import kotlinx.android.synthetic.main.fragment_career1.*
 import org.jetbrains.anko.find
 import org.jetbrains.anko.support.v4.startActivity
+import android.content.Intent
+import org.jetbrains.anko.support.v4.toast
+
 
 class Career1Fragment : Fragment() {
+
+
     private var career1Fragment: View? = null
     lateinit var careerRecyclerViewAdapter: CareerRecyclerViewAdapter
-    private var titleTxt : String? = null
-    private var activityDate : String? = null
-    private var contentTxt : String? = null
-
-
+    private var titleTxt: String? = null
+    private var activityDate: String? = null
+    private var contentTxt: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //bundle
-        arguments?.let{
-            titleTxt = it.getString("title")
-            activityDate = it.getString("date")
-            contentTxt = it.getString("content")
-        }
+
     }
 
     // bundle
-    companion object {
-        private var instance: Career1Fragment? = null
-        @Synchronized
-        fun getInstance(titleTxt: String, activityDate: String, contentTxt: String): Career1Fragment{
-            if(instance==null){
-                instance = Career1Fragment().apply{
-                    arguments = Bundle().apply{
-                        putString("title", titleTxt)
-                        putString("date", activityDate)
-                        putString("content", contentTxt)
-                    }
-                }
-            }
-            return instance!!
-        }
-    }
+//    companion object {
+//        private var instance: Career1Fragment? = null
+//        @Synchronized
+//        fun getInstance(titleTxt: String, activityDate: String, contentTxt: String): Career1Fragment {
+//            if (instance == null) {
+//                instance = Career1Fragment().apply {
+//                    arguments = Bundle().apply {
+//                        putString("title", titleTxt)
+//                        putString("date", activityDate)
+//                        putString("content", contentTxt)
+//                    }
+//                }
+//            }
+//            return instance!!
+//        }
+//    }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         career1Fragment = inflater!!.inflate(R.layout.fragment_career1, container, false)
         setBtnOnClickListener()
@@ -68,37 +66,54 @@ class Career1Fragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        setRecyclerView()
-
-
-        //recycler view에 아이템 추가함
-        val position: Int = careerRecyclerViewAdapter.itemCount
-        careerRecyclerViewAdapter.dataList.add(CareerListData(titleTxt, activityDate, contentTxt))
-        careerRecyclerViewAdapter.notifyItemInserted(position)
-
-
+//        setRecyclerView()
+//
+//        //recycler view에 아이템 추가함
+//        val position: Int = careerRecyclerViewAdapter.itemCount
+//        careerRecyclerViewAdapter.dataList.add(CareerListData(titleTxt, activityDate, contentTxt))
+//        careerRecyclerViewAdapter.notifyItemInserted(position)
     }
 
 
-    private fun setRecyclerView(){
+    private fun setRecyclerView() {
         var dataList: ArrayList<CareerListData> = ArrayList()
         dataList.add(CareerListData("안녕하세요", "2018.10", "이런 활동을 했습니다"))
+
+        Log.e("data list size", "aaaaaaa" + dataList.size)
 
         careerRecyclerViewAdapter = CareerRecyclerViewAdapter(activity!!, dataList)
         rv_career1_frag_activity_list.adapter = careerRecyclerViewAdapter
         rv_career1_frag_activity_list.layoutManager = LinearLayoutManager(activity)
-
     }
 
-    private fun setBtnOnClickListener(){
+    private fun setBtnOnClickListener() {
         val add_activity: RelativeLayout = career1Fragment!!.find(R.id.btn_add_activity)
         add_activity.setOnClickListener {
-            startActivity<OutActivityDetail>()
-        }
 
+            startActivity<OutActivityDetail>()
+
+
+        }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.e("졸려워", "진짜로")
+        Log.e("진희야", "힘을내")
 
+        val fragments = fragmentManager!!.fragments
 
+        if (fragments != null) {
+            for (fragment in fragments) {
+                fragment.onActivityResult(requestCode, resultCode, data)
 
+                arguments?.let {
+                    titleTxt = it.getString("title")
+//            activityDate = it.getString("date")
+//            contentTxt = it.getString("content")
+                }
+                Log.e("졸려워", "진짜로" + titleTxt.toString())
+            }
+        }
+    }
 }
